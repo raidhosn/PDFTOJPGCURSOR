621d1d25def7d2c53cadbf299309aaceb220daea
Raid Aboul Hosn <raidhosn1972@gmail.com>
2026-01-27 23:58:38 -0800

    Build to docs folder for GitHub Pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/PDFTOJPGCURSOR/",
+  build: {
+    outDir: "docs"
+  },
   server: {
     port: 5173,
     strictPort: false,
